fda25cd1106e1234946ff22ca0a3ea27579e3e69
Rory Abraham <rory@expensify.com>
2022-02-16 21:21:56 -0800

    Fix comments that reference top and bottom

--- Libraries/Lists/VirtualizedList.js
+++ Libraries/Lists/VirtualizedList.js
@@ -215,9 +215,9 @@ type OptionalProps = {|
    */
   onStartReached?: ?(info: {distanceFromStart: number, ...}) => void,
   /**
-   * How far from the start (in units of visible length of the list) the top edge of the
+   * How far from the start (in units of visible length of the list) the leading edge of the
    * list must be from the start of the content to trigger the `onStartReached` callback.
-   * Thus, a value of 0.5 will trigger `onStartReached` when the end of the content is
+   * Thus, a value of 0.5 will trigger `onStartReached` when the start of the content is
    * within half the visible length of the list.
    */
   onStartReachedThreshold?: ?number,
@@ -227,7 +227,7 @@ type OptionalProps = {|
    */
   onEndReached?: ?(info: {distanceFromEnd: number, ...}) => void,
   /**
-   * How far from the end (in units of visible length of the list) the bottom edge of the
+   * How far from the end (in units of visible length of the list) the trailing edge of the
    * list must be from the end of the content to trigger the `onEndReached` callback.
    * Thus, a value of 0.5 will trigger `onEndReached` when the end of the content is
    * within half the visible length of the list.
